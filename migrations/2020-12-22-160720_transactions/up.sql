81448150aef149d9fc405b2a2b07ba95a182073f
Luís Mendes <luisgbm@gmail.com>
2021-01-06 16:50:13 -0300

    Cascade delete transactions when deleting categories and accounts

--- migrations/2020-12-22-160720_transactions/up.sql
+++ migrations/2020-12-22-160720_transactions/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE transactions(
-    id          SERIAL PRIMARY KEY                NOT NULL,
-    value       INTEGER                           NOT NULL,
-    description TEXT                              NOT NULL,
-    date        TIMESTAMP                         NOT NULL,
-    account     INTEGER REFERENCES accounts(id)   NOT NULL,
-    category    INTEGER REFERENCES categories(id) NOT NULL
+                             id          SERIAL PRIMARY KEY                                   NOT NULL,
+                             value       INTEGER                                              NOT NULL,
+                             description TEXT                                                 NOT NULL,
+                             date        TIMESTAMP                                            NOT NULL,
+                             account     INTEGER REFERENCES accounts (id) ON DELETE CASCADE   NOT NULL,
+                             category    INTEGER REFERENCES categories (id) ON DELETE CASCADE NOT NULL
 );

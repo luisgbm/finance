546d597d7f056174fb91db626726943a0bda5947
Luís Mendes <luisgbm@gmail.com>
2021-01-20 18:29:01 -0300

    Testing authentication route guard for post category

--- src/routes.rs
+++ src/routes.rs
@@ -1,15 +1,58 @@
 use diesel::result::{DatabaseErrorKind, Error};
+use jsonwebtoken::TokenData;
 use rocket;
 use rocket::http::Status;
+use rocket::outcome::Outcome::*;
+use rocket::request::{self, FromRequest, Request};
 use rocket_contrib::json::Json;
 
 use crate::finance_db::FinanceDB;
 use crate::jwt;
+use crate::jwt::Claims;
 use crate::models::{Account, AccountWithBalance, Category,
                     CategoryTypes, NewAccount, NewCategory,
                     NewTransaction, NewUser, Transaction, TransactionJoined,
                     TransactionNoAccount, User};
 use crate::utils;
+
+#[derive(Debug)]
+pub struct Authentication {
+    token: TokenData<Claims>
+}
+
+#[derive(Debug)]
+pub enum AuthenticationError {
+    Missing,
+    Invalid,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Authentication {
+    type Error = AuthenticationError;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let min_header_len = "Bearer ".len();
+
+        let auth_header = request.headers().get_one("Authorization");
+
+        match auth_header {
+            None => Failure((Status::Unauthorized, AuthenticationError::Missing)),
+            Some(auth_header) => {
+                if auth_header.len() < min_header_len {
+                    return Failure((Status::Unauthorized, AuthenticationError::Invalid));
+                }
+
+                match jwt::validate_jwt(&auth_header[min_header_len..].to_string()) {
+                    Ok(token) => {
+                        Success(Authentication {
+                            token
+                        })
+                    }
+                    Err(_) => Failure((Status::Unauthorized, AuthenticationError::Invalid))
+                }
+            }
+        }
+    }
+}
 
 #[post("/login", format = "json", data = "<user>")]
 pub fn login(user: Json<NewUser>) -> Result<String, Status> {
@@ -63,7 +106,7 @@ pub fn post_account(account: Json<NewAccount>) -> Json<Account> {
 }
 
 #[post("/categories", format = "json", data = "<category>")]
-pub fn post_category(category: Json<NewCategory>) -> Json<Category> {
+pub fn post_category(category: Json<NewCategory>, _auth: Authentication) -> Json<Category> {
     Json(FinanceDB::new().new_category(&category.into_inner()))
 }
 

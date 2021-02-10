5c3d65d8d66ec76ff5949ddd47ffef396cfb9488
Luís Mendes <luisgbm@gmail.com>
2021-02-10 15:28:47 -0300

    CRUD database operations for scheduled transactions

--- src/finance_db.rs
+++ src/finance_db.rs
@@ -6,7 +6,7 @@ use diesel::prelude::*;
 use diesel::result::Error;
 use dotenv::dotenv;
 
-use crate::models::{Account, AppUser, Category, CategoryTypes, NewAccount, NewAppUser, NewCategory, NewTransaction, NewTransfer, Transaction, Transfer};
+use crate::models::{Account, AppUser, Category, CategoryTypes, NewAccount, NewAppUser, NewCategory, NewScheduledTransaction, NewTransaction, NewTransfer, ScheduledTransaction, Transaction, Transfer};
 
 pub struct FinanceDB {
     connection: PgConnection
@@ -48,6 +48,15 @@ impl FinanceDB {
                 app_users::password.eq(crypt(new_user.password.clone(), gen_salt("bf", bf_rounds)))
             ))
             .get_result(&self.connection)
+    }
+
+    pub fn new_scheduled_transaction(&self, new_scheduled_transaction: &NewScheduledTransaction) -> ScheduledTransaction {
+        use crate::schema::scheduled_transactions;
+
+        diesel::insert_into(scheduled_transactions::table)
+            .values(new_scheduled_transaction)
+            .get_result(&self.connection)
+            .expect("Error saving new scheduled transaction")
     }
 
     pub fn new_transfer(&self, new_transfer: &NewTransfer) -> Transfer {
@@ -113,6 +122,19 @@ impl FinanceDB {
         }
     }
 
+    pub fn get_all_scheduled_transactions(&self, app_user_id: i32) -> QueryResult<Vec<(ScheduledTransaction, Category, Account, AppUser)>> {
+        use crate::schema::scheduled_transactions::dsl::*;
+        use crate::schema::scheduled_transactions;
+        use crate::schema::categories;
+        use crate::schema::accounts;
+        use crate::schema::app_users;
+
+        scheduled_transactions::table.inner_join(categories::table).inner_join(accounts::table).inner_join(app_users::table)
+            .filter(user_id.eq(app_user_id))
+            .order(date.asc())
+            .load::<(ScheduledTransaction, Category, Account, AppUser)>(&self.connection)
+    }
+
     pub fn get_all_transactions_of_account_joined(&self, account_id: i32, app_user_id: i32) -> Vec<(Transaction, Category, Account)> {
         use crate::schema::transactions::dsl::*;
         use crate::schema::transactions;
@@ -175,6 +197,19 @@ impl FinanceDB {
             .expect("Error loading transfers")
     }
 
+    pub fn get_scheduled_transaction(&self, find_id: i32, app_user_id: i32) -> QueryResult<(ScheduledTransaction, Category, Account, AppUser)> {
+        use crate::schema::scheduled_transactions::dsl::*;
+        use crate::schema::scheduled_transactions;
+        use crate::schema::categories;
+        use crate::schema::accounts;
+        use crate::schema::app_users;
+
+        scheduled_transactions::table.inner_join(categories::table).inner_join(accounts::table).inner_join(app_users::table)
+            .filter(user_id.eq(app_user_id))
+            .filter(id.eq(find_id))
+            .first::<(ScheduledTransaction, Category, Account, AppUser)>(&self.connection)
+    }
+
     pub fn get_transaction(&self, find_id: i32, app_user_id: i32) -> Result<(Transaction, Category, Account), Error> {
         use crate::schema::transactions::dsl::*;
         use crate::schema::transactions;
@@ -212,6 +247,25 @@ impl FinanceDB {
             .filter(user_id.eq(app_user_id))
             .find(find_id)
             .first::<Category>(&self.connection)
+    }
+
+    pub fn update_scheduled_transaction(&self, update_id: i32, update_scheduled_transaction: &NewScheduledTransaction, app_user_id: i32) -> QueryResult<ScheduledTransaction> {
+        use crate::schema::scheduled_transactions::dsl::*;
+
+        diesel::update(scheduled_transactions.filter(user_id.eq(app_user_id)).find(update_id))
+            .set((
+                account_id.eq(update_scheduled_transaction.account_id),
+                value.eq(update_scheduled_transaction.value),
+                description.eq(update_scheduled_transaction.description),
+                category_id.eq(update_scheduled_transaction.category_id),
+                date.eq(update_scheduled_transaction.date),
+                repeat.eq(update_scheduled_transaction.repeat),
+                repeat_freq.eq(update_scheduled_transaction.repeat_freq),
+                repeat_interval.eq(update_scheduled_transaction.repeat_interval),
+                end_after_repeats.eq(update_scheduled_transaction.end_after_repeats),
+                current_repeat_count.eq(update_scheduled_transaction.current_repeat_count)
+            ))
+            .get_result::<ScheduledTransaction>(&self.connection)
     }
 
     pub fn update_transaction(&self, update_id: i32, update_transaction: &NewTransaction, app_user_id: i32) -> Result<Transaction, Error> {
@@ -256,6 +310,13 @@ impl FinanceDB {
                 date.eq(update_transfer.date)
             ))
             .get_result::<Transfer>(&self.connection)
+    }
+
+    pub fn delete_scheduled_transaction(&self, delete_id: i32, app_user_id: i32) -> QueryResult<ScheduledTransaction> {
+        use crate::schema::scheduled_transactions::dsl::*;
+
+        diesel::delete(scheduled_transactions.filter(user_id.eq(app_user_id)).find(delete_id))
+            .get_result::<ScheduledTransaction>(&self.connection)
     }
 
     pub fn delete_transaction(&self, delete_id: i32, app_user_id: i32) -> Result<Transaction, Error> {

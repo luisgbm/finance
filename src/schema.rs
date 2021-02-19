af488554eaebfd62ad24eda8e9c2b24f3f5981f9
Luís Mendes <luisgbm@gmail.com>
2021-02-19 13:42:07 -0300

    Schema for scheduled transfers

--- src/schema.rs
+++ src/schema.rs
@@ -80,6 +80,28 @@ table! {
     }
 }
 
+table! {
+    use crate::models_db::RepeatFrequenciesMapping;
+    use diesel::sql_types::{Integer, Text, Timestamp, Bool, Nullable};
+
+    scheduled_transfers (id) {
+        id -> Integer,
+        origin_account_id -> Integer,
+        destination_account_id -> Integer,
+        value -> Integer,
+        description -> Text,
+        created_date -> Timestamp,
+        repeat -> Bool,
+        repeat_freq -> Nullable<RepeatFrequenciesMapping>,
+        repeat_interval -> Nullable<Integer>,
+        infinite_repeat -> Nullable<Bool>,
+        end_after_repeats -> Nullable<Integer>,
+        current_repeat_count -> Nullable<Integer>,
+        next_date -> Nullable<Timestamp>,
+        user_id -> Integer,
+    }
+}
+
 joinable!(transactions -> accounts (account));
 joinable!(transactions -> categories (category));
 joinable!(transactions -> app_users (user_id));
@@ -90,6 +112,8 @@ joinable!(transfers -> accounts (origin_account));
 joinable!(scheduled_transactions -> accounts (account_id));
 joinable!(scheduled_transactions -> categories (category_id));
 joinable!(scheduled_transactions -> app_users (user_id));
+joinable!(scheduled_transfers -> accounts (origin_account_id));
+joinable!(scheduled_transfers -> app_users (user_id));
 
 allow_tables_to_appear_in_same_query!(
     accounts,
@@ -97,5 +121,6 @@ allow_tables_to_appear_in_same_query!(
     transactions,
     app_users,
     transfers,
-    scheduled_transactions
+    scheduled_transactions,
+    scheduled_transfers
 );
